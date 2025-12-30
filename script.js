152f67e5403213cccee7d3f8fd2d906dd841263d
delgadojose2500 <50949199+delgadojose2500@users.noreply.github.com>
2025-12-30 12:49:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <svg class="lock-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M12 17a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm6-9h-1V6a5 5 0 0 0-10 0v2H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2zM9 6a3 3 0 1 1 6 0v2H9V6z"/>
                 </svg>
-                Pista Bloqueada
+                Todavía no kk jejeje
             `;
             if (unlockMessage) unlockMessage.style.display = 'none';
         }
@@ -464,4 +464,5 @@ document.addEventListener('DOMContentLoaded', () => {
         startGameBtn.textContent = "Reintentar";
     }
 });
+
 
